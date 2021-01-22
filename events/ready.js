fee012aa0030fcaeb21661289aa56ed6e6c2b8fa
esaicornejo1 <esaicornejo1@gmail.com>
2021-01-22 16:48:56 +0000

    Bug Fixes 2.3

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("BETA", {
+  await client.user.setActivity("BETA V2.2", {
     type: "WATCHING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
